3c85d559385145f55b1300199c7fccc38223c52c
Yitong Zhao <92388449+yitZhao21@users.noreply.github.com>
2025-01-31 11:53:41 -0500

    Update assignment2.sql
    I have completed assignment 2 (sql portion) and added in my code.

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -20,7 +20,14 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
+SELECT 
+product_name, product_size, product_qty_type
+FROM product
+WHERE NULLIF(product_size, '') IS NULL;
 
+SELECT 
+product_name || ', ' || IFNULL(product_size,' ')|| ' (' || IFNULL(product_qty_type,'Unit') || ')'
+FROM product;
 
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
@@ -32,17 +39,30 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
-
+SELECT DISTINCT *,ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY market_date, transaction_time ASC) as num_visit
+FROM customer_purchases;
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+DROP TABLE IF EXISTS temp.customer_Visits; 
+CREATE TEMP TABLE customer_Visits AS
+SELECT DISTINCT *,ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY market_date, transaction_time DESC) as num_visit
+FROM customer_purchases;
 
+SELECT * FROM customer_Visits WHERE num_visit=1;
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
 
+SELECT DISTINCT customer_id, product_id, COUNT(product_id) OVER(PARTITION BY customer_id ORDER BY market_date DESC) as quantity 
+FROM customer_purchases;
+
+-- how many products per product_qty_type AND per their product_size
+
+SELECT DISTINCT IFNULL(product_size, 'missing'), IFNULL(product_qty_type, 'missing'), COUNT(product_id) OVER(PARTITION BY product_size, product_qty_type ORDER BY product_name ASC) as product_quantity 
+FROM product;
 
 
 -- String manipulations
@@ -57,25 +77,70 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
+SELECT *,
+SUBSTR(product_name,INSTR(product_name,'-')+2)
+FROM product;
 
+SELECT *, 
+CASE WHEN SUBSTR(product_name,INSTR(product_name,'-')+2) = 'Organic' 
+		THEN 'Organic'
+	WHEN SUBSTR(product_name,INSTR(product_name,'-')+2) = 'Jar'
+		THEN 'Jar'
+	ELSE 'NULL'
+	   END as description
+FROM product;
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
-
-
+SELECT * FROM product
+WHERE product_size REGEXP '\d';
 
 -- UNION
 /* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
 
 HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
-1) Create a CTE/Temp Table to find sales values grouped dates; 
+1) Create a CTE/Temp Table to find sales values grouped by dates; 
 2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
 "best day" and "worst day"; 
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
 
+CREATE TEMP TABLE Daily_Sales AS
+SELECT DISTINCT *,SUM(quantity*cost_to_customer_per_qty) OVER(PARTITION BY market_date) as DailySales
+FROM customer_purchases;
 
 
+CREATE TEMP TABLE DailySales_Max AS
+SELECT *, 
+FROM (
+	SELECT DISTINCT
+	*,
+	DENSE_RANK() OVER(ORDER BY DailySales DESC) as Rank_Max
+	FROM Daily_Sales
+) x
+WHERE x.Rank_Max = 1;
 
+
+CREATE TEMP TABLE DailySales_Min AS
+SELECT *
+FROM (
+	SELECT DISTINCT
+	*,
+	DENSE_RANK() OVER(ORDER BY DailySales ASC) as Rank_Min
+	FROM Daily_Sales
+) x
+WHERE x.Rank_Min = 1;
+	
+	
+SELECT market_date, DailySales
+FROM DailySales_Max
+
+UNION
+
+SELECT market_date, DailySales
+FROM DailySales_Min;
+	
+	
+	
 /* SECTION 3 */
 
 -- Cross Join
@@ -89,6 +154,49 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
+SELECT * FROM product;
+SELECT * FROM v;
+
+-- grab original price of each product
+DROP TABLE IF EXISTS temp.vendor_product;
+CREATE TEMP TABLE vendor_product AS
+SELECT * FROM vendor_inventory as v
+INNER JOIN product as p
+ON v. product_id = p.product_id;
+
+-- grab names of each vendor
+DROP TABLE IF EXISTS temp.vendor_product_name;
+CREATE TEMP TABLE vendor_product_name AS
+SELECT * FROM vendor_product as vp
+INNER JOIN vendor as v
+ON vp.vendor_id = v.vendor_id;
+
+--grab only relevant info
+DROP TABLE IF EXISTS temp.vendor_product_needed;
+CREATE TEMP TABLE vendor_product_needed AS
+SELECT DISTINCT vendor_name, product_name, original_price FROM vendor_product_name;
+
+--grab customer names only
+DROP TABLE IF EXISTS temp.customerUnique;
+CREATE TEMP TABLE customerUnique AS
+SELECT customer_first_name || ' ' || customer_last_name as customer_name FROM customer;
+
+-- Got the final combo before computing prices*quantity
+DROP TABLE IF EXISTS temp.final;
+CREATE TEMP TABLE final AS
+SELECT * FROM vendor_product_needed
+CROSS JOIN customerUnique;
+
+-- Compute and group by vendor
+
+SELECT * SUM(5*original_price) as sales_per_product 
+FROM final
+GROUP BY cp.customer_id, customer_last_name, customer_first_name
+HAVING price >= 2000;
+
+--- RESULTS SEE HERE:
+SELECT DISTINCT *, SUM(5*original_price) OVER(PARTITION BY vendor_name ORDER BY product_name) as vendor_sales 
+FROM final;
 
 
 -- INSERT
@@ -97,18 +205,26 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
+DROP TABLE IF EXISTS temp.unit_product;
+CREATE TEMP TABLE IF NOT EXISTS temp.unit_product AS
+SELECT *, CURRENT_TIMESTAMP as snapshot_timestamp FROM products WHERE product_qty_type = 'unit';
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
-
+INSERT INTO unit_product
+VALUES(6, 'Red Pepper Jam', '8 oz', 12, 'unit', CURRENT_TIMESTAMP);
+SELECT * FROM unit_product;
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
+DELETE FROM unit_product -- finally, run with this
+WHERE product_id=6;
+SELECT * FROM unit_product;
 
 
 -- UPDATE
@@ -129,5 +245,33 @@ Finally, make sure you have a WHERE statement to update the right row,
 When you have all of these components, you can run the update statement. */
 
 
+ALTER TABLE unit_product
+ADD current_quantity INT;
 
+DROP TABLE IF EXISTS all_last_quantity;
+CREATE TEMP TABLE all_last_quantity AS
+SELECT DISTINCT *,SUM(quantity) OVER(PARTITION BY product_id) as last_quantity
+FROM vendor_inventory;
 
+DROP TABLE IF EXISTS uniqe_last_quantity;
+CREATE TEMP TABLE uniqe_last_quantity AS
+SELECT DISTINCT product_id, last_quantity FROM all_last_quantity;
+
+--coalesce
+DROP TABLE IF EXISTS uniqe_last_quantity_new;
+CREATE TEMP TABLE uniqe_last_quantity_new AS
+SELECT product_id, product_name, product_size, product_category_id, product_qty_type, snapshot_timestamp, coalesce(current_quantity,0) as current_quantity FROM unit_product;
+
+-- add last quantity
+DROP TABLE IF EXISTS final_update;
+CREATE TEMP TABLE final_update AS
+SELECT * FROM uniqe_last_quantity_new as up_new
+INNER JOIN uniqe_last_quantity as alq ON up_new.product_id = alq.product_id;
+
+--change the product_size for almonds from 1lb to 1/2kg
+SELECT * FROM final_update;
+UPDATE final_update
+SET current_quantity = last_quantity;
+
+-- updated table where current quantity is the same as last quantity
+SELECT * FROM final_update;
